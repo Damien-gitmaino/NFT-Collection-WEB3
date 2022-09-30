3460f765fc16f174f9eed3ae88552055a58f8b0b
Damien-EpiMaillard <damien.maillard@epitech.eu>
2022-09-29 22:43:52 -0400

    first commit

--- hardhat-tutorial/constants/index.js
+++ hardhat-tutorial/constants/index.js
@@ -1,6 +1,6 @@
 // Address of the Whitelist Contract that you deployed
 const WHITELIST_CONTRACT_ADDRESS = "0x760c038DAb10F0eb1B2cCBa51B8C1Caeb8f77cE8";
 // URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+const METADATA_URL = "https://nft-collection-web-3.vercel.app/api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
